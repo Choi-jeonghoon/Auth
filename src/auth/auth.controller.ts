bcccefb019cabcdbfcb072ee28c7198283297deb
Choi-jeonghoon <jeongssi94@gmail.com>
2024-12-04 02:12:07 +0900

    Ref: Jwt passport 적용해보기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Headers, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 // import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './strategy/local.strategy';
+import { JwtAuthGuard } from './strategy/jwt.strategy';
 
 @Controller('auth')
 export class AuthController {
@@ -31,7 +39,16 @@ export class AuthController {
    */
   @UseGuards(LocalAuthGuard)
   @Post('login/passport')
-  loginUserPassport(@Request() req) {
+  async loginUserPassport(@Request() req) {
+    return {
+      refreshToken: await this.authService.issueToken(req.user, true), // accessToken을 재발급하기 위한 것
+      accessToken: await this.authService.issueToken(req.user, false), // 사용자가 로그인된 상태임을 증명하고, 권한 및 인증 정보를 확인하기 위해 발급된 토큰
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('private')
+  async private(@Request() req) {
     return req.user;
   }
 }
